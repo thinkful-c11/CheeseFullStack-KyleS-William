6650219836ee6dbed45ff085f0c43ae632b69100
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-06-30 08:32:29 -0700

    fixed =.=

--- client/src/cheese-list.js
+++ client/src/cheese-list.js
@@ -4,7 +4,7 @@ export default class CheeseList extends React.Component{
   render(){
     console.log(this.props.cheeses);
     const cheeseList = this.props.cheeses.map((cheese,ind)=>{
-      <li key={ind} index={ind}>{cheese}</li>
+      return <li key={ind} index={ind}>{cheese}</li>
     });
     console.log(cheeseList);
     return (
