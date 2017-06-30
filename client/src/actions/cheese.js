ce55693ad6ff070a6a3e9241f858b17fc56120eb
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-06-30 12:09:09 -0700

    changed to heroku

--- client/src/actions/cheese.js
+++ client/src/actions/cheese.js
@@ -18,8 +18,8 @@ export const cheeseFailure = (error) => ({
 
 export const fetchCheeses = () => (dispatch) => {
   dispatch(cheeseRequest());
-  //return fetch('https://hidden-mountain-34149.herokuapp.com/api/cheeses')
-  return fetch('http://localhost:8080/api/cheeses')
+  return fetch('https://hidden-mountain-34149.herokuapp.com/api/cheeses')
+  //return fetch('http://localhost:8080/api/cheeses')
   .then(res=>{
     if(!res.ok){
       return Promise.reject(res.statusText);
@@ -36,7 +36,8 @@ export const fetchCheeses = () => (dispatch) => {
 
 export const addCheese = (cheese) => (dispatch) => {
   dispatch(cheeseRequest());
-  return fetch('http://localhost:8080/api/cheeses', {
+  return fetch('https://hidden-mountain-34149.herokuapp.com/api/cheeses', {
+  //return fetch('http://localhost:8080/api/cheeses', {
     method: 'POST',
     mode: 'cors',
     headers: {
